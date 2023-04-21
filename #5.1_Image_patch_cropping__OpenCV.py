d96ffb51fe5b2a2904189688e3f39e2fc0e88793
steinerML <102678863+steinerML@users.noreply.github.com>
2023-04-21 12:55:25 +0200

    #5 is image cropping and #5.1 is patching.

---
+++ #5.1_Image_patch_cropping__OpenCV.py
@@ -0,0 +1,67 @@
+import cv2
+import numpy as np
+
+#google colab here: shorturl.at/aj457
+
+#Read cropped image from #5
+img =  cv2.imread("test_cropped.jpg")
+
+#We get the height and width from the cropped image so we can use it later for dividing the image in patches
+image_copy = img.copy() 
+imgheight=img.shape[0]
+imgwidth=img.shape[1]
+#We print the image size just to avoid going back to the folder
+print(imgwidth)
+print(imgheight)
+
+#We crop using 2 for loops.
+
+M = 130
+N = 140
+x1 = 0
+y1 = 0
+
+#Whenever I have a minute I need to check this again!!
+for y in range(0, imgheight, M):
+    for x in range(0, imgwidth, N):
+        if (imgheight - y) < M or (imgwidth - x) < N:
+            break             
+        y1 = y + M
+        x1 = x + N
+ 
+        # check whether the patch width or height exceeds the image width or height
+        if x1 >= imgwidth and y1 >= imgheight:
+            x1 = imgwidth - 1
+            y1 = imgheight - 1
+            #Crop into patches of size MxN
+            tiles = image_copy[y:y+M, x:x+N]
+            #Save each patch into file directory
+            cv2.imwrite('saved_patches/'+'tile'+str(x)+'_'+str(y)+'.jpg', tiles)
+            cv2.rectangle(img, (x, y), (x1, y1), (0, 255, 0), 1)
+        elif y1 >= imgheight: # when patch height exceeds the image height
+            y1 = imgheight - 1
+            #Crop into patches of size MxN
+            tiles = image_copy[y:y+M, x:x+N]
+            #Save each patch into file directory
+            cv2.imwrite('saved_patches/'+'tile'+str(x)+'_'+str(y)+'.jpg', tiles)
+            cv2.rectangle(img, (x, y), (x1, y1), (0, 255, 0), 1)
+        elif x1 >= imgwidth: # when patch width exceeds the image width
+            x1 = imgwidth - 1
+            #Crop into patches of size MxN
+            tiles = image_copy[y:y+M, x:x+N]
+            #Save each patch into file directory
+            cv2.imwrite('saved_patches/'+'tile'+str(x)+'_'+str(y)+'.jpg', tiles)
+            cv2.rectangle(img, (x, y), (x1, y1), (0, 255, 0), 1)
+        else:
+            #Crop into patches of size MxN
+            tiles = image_copy[y:y+M, x:x+N]
+            #Save each patch into file directory
+            cv2.imwrite('saved_patches/'+'tile'+str(x)+'_'+str(y)+'.jpg', tiles)
+            cv2.rectangle(img, (x, y), (x1, y1), (0, 255, 0), 1)
+
+#Save full image into file directory
+cv2.imshow("Patched Image",img)
+cv2.imwrite("patched.jpg",img)
+  
+cv2.waitKey(0)
+cv2.destroyAllWindows()
